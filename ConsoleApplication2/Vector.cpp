a2034acf07d18965b8201cbfbeadf7e8875e2e80
MartinApaza <a2025112301@estudiantes.upsa.edu.bo>
2026-03-09 13:49:40 -0400

    Add files via upload

---
+++ ConsoleApplication2/Vector.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include "vector.h"
+
+using namespace std;
+
+Vector::Vector(void)
+{
+	vec[10] = 0;
+	n = 0;
+}
+
+
+Vector::~Vector(void)
+{
+}
+
+void Vector::cargarVector(int vec[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "vec[" << i << "] =";
+		cin >> vec[i];
+	}
+}
+
+void Vector::mostrarVector(int vec[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << vec[i] << ", ";
+	}
+	cout << endl;
+}
+
+void Vector::ordenarVector(int vec[], int n) {
+	int aux;
+	for (int i = 0; i < (n - 1); i++) {
+		for (int j = i; j < n; j++) {
+			if (vec[i] > vec[j]) {
+				aux = vec[i];
+				vec[i] = vec[j];
+				vec[j] = aux;
+			}
+		}
+	}
+}
